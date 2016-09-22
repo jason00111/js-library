0e0784a134c0cd2c269a567ccb13da5dad77d693
nicosesma <nicosm310@gmail.com>
2016-09-22 14:51:27 -0700

    started head implementation

---
+++ src/spec/headSpec.js
@@ -0,0 +1,16 @@
+const chai = require('chai')
+const head = chai.expect
+
+import head from '..functions/head.js'
+
+describe('head', () => {
+  it('finds the first element of an array', () => {
+    expect( head([1,2,3,4])).to.equal(1)
+  })
+  it('finds the first character in a string', () => {
+    expect( head('string')).to.equal('s')
+  })
+  it('returns undefined if input is not a string or an array', () => {
+    expect( head(true)).to.be.undefined
+  })
+})
